47e39c0f17b0c9bdb1376f0493bc67666735a831
Jeremias Quinteros <quinterosjeremias8@gmail.com>
2024-07-22 11:25:10 -0300

    agregamos un .env

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Hola " + process.NOMBRE_USUARIO +"!"));
 
 app.post("/users", (req, res)=>{
-    res.send('POST de users ejecutado exitosamente!')
+    res.send('POST de users ejecutado exitosamente!');
 })
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
